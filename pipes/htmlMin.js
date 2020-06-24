539ac5b2095e006c145b452ba8d65a75feb47d4e
Grigorii Horos <horosgrisa@gmail.com>
2020-06-24 22:35:47 +0300

    Optimize

--- pipes/htmlMin.js
+++ pipes/htmlMin.js
@@ -4,7 +4,7 @@ import minifier from 'html-minifier';
  * @param response
  */
 export async function pipeHtmlMin(response) {
-  if (response?.header['Content-Type']?.startsWith('text/html') || response?.header['content-type']?.startsWith('text/html')) {
+  if (response?.header['Content-Type']?.startsWith('text/html')) {
     const bodyString = response.body.toString();
     const lines = (bodyString.match(/\n/g) || '').length + 1;
 
