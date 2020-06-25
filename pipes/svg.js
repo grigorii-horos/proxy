15edeb6b7273e8a2c61d8a14e7ba62253eab54c1
Grigorii Horos <horosgrisa@gmail.com>
2020-06-26 02:30:58 +0300

    Optimize

--- pipes/svg.js
+++ pipes/svg.js
@@ -14,25 +14,31 @@ export async function pipeSvg(response, request) {
     response?.header['Content-Type']?.startsWith('image/svg+xml')
   ) {
     const oldSize = response.body.length;
+    try {
+      const svg = await svgo.optimize(response.body);
+      const newSize = svg.data.length;
 
-    const svg = await svgo.optimize(response.body);
-    const newSize = svg.data.length;
+      console.log(
+        `Compres Image: Old - ${
+          prettyBytes(oldSize)
+        } New - ${
+          prettyBytes(newSize)
+        } Compression - ${
+          Math.floor((100 * newSize) / oldSize)
+        }%`,
+      );
 
-    console.log(
-      `Compres Image: Old - ${
-        prettyBytes(oldSize)
-      } New - ${
-        prettyBytes(newSize)
-      } Compression - ${
-        Math.floor((100 * newSize) / oldSize)
-      }%`,
-    );
+      if (svg.data.length < 10) {
+        return response;
+      }
 
-    return {
-      ...response,
-      body: svg.data,
-
-    };
+      return {
+        ...response,
+        body: svg.data,
+      };
+    } catch (error) {
+      return response;
+    }
   }
 
   return response;
