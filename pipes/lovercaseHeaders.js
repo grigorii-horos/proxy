582560e2331084cc24a2ecbf15116a909b4bec4c
Grigorii Horos <horosgrisa@gmail.com>
2020-06-28 15:44:04 +0300

    Optimize

--- pipes/lovercaseHeaders.js
+++ pipes/lovercaseHeaders.js
@@ -4,7 +4,7 @@ import lowercaseKeys from 'lowercase-keys';
  * @param response
  */
 export async function pipeLovercaseHeader(response) {
-  const headers = lowercaseKeys(response.header);
+  const headers = lowercaseKeys(response.header || {});
 
   return {
     ...response,
