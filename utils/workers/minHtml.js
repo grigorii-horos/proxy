70e47567601ed43f7a7d99697cc20d4b192e6708
Grigorii Horos <horosgrisa@gmail.com>
2020-06-28 23:20:26 +0300

    Optimize

--- utils/workers/minHtml.js
+++ utils/workers/minHtml.js
@@ -7,21 +7,21 @@ parentPort.once('message', async (data) => {
   let bodyString = replaceAll(data, '<img', '<img loading="lazy"');
   bodyString = replaceAll(bodyString, '<iframe', '<iframe loading="lazy"');
 
-  bodyString = minifier.minify(bodyString, {
-    collapseBooleanAttributes: true,
-    collapseWhitespace: true,
-    conservativeCollapse: true,
-    continueOnParseError: true,
-    decodeEntities: true,
-    keepClosingSlash: false,
-    minifyCSS: true,
-    minifyJS: true,
-    removeAttributeQuotes: true,
-    removeComments: true,
-    removeEmptyAttributes: true,
-    sortAttributes: false,
-    sortClassName: false,
-  });
+  // bodyString = minifier.minify(bodyString, {
+  //   collapseBooleanAttributes: true,
+  //   collapseWhitespace: true,
+  //   conservativeCollapse: true,
+  //   continueOnParseError: true,
+  //   decodeEntities: true,
+  //   keepClosingSlash: false,
+  //   minifyCSS: true,
+  //   minifyJS: true,
+  //   removeAttributeQuotes: true,
+  //   removeComments: true,
+  //   removeEmptyAttributes: true,
+  //   sortAttributes: false,
+  //   sortClassName: false,
+  // });
 
   parentPort.postMessage({
   // @ts-ignore
