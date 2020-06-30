e653edbd059da349e682329289e3736e613abef4
Grigorii Horos <horosgrisa@gmail.com>
2020-07-01 02:35:38 +0300

    Optimize

--- pipes/losslessImage.js
+++ pipes/losslessImage.js
@@ -1,6 +1,5 @@
 import prettyBytes from 'pretty-bytes';
-
-import { imageFn } from '../utils/losslessImage.js';
+import sharp from 'sharp';
 
 const imageMimeTypes = [
   'image/png',
@@ -15,7 +14,23 @@ export async function pipeLosslessImage(response, request) {
   if (
     imageMimeTypes.includes(response?.header['content-type'])
   ) {
-    const newBody = imageFn(response.body);
+    let image = sharp(await response.body);
+
+    const metadata = await image.metadata();
+
+    if (metadata.width > 1500) {
+      image = image.resize(Math.round(metadata.width / 1.5));
+    } else if (metadata.width > 2500) {
+      image = image.resize(Math.round(metadata.width / 2));
+    }
+
+    const newBody = image
+      .toFormat('webp', {
+        lossless: false,
+        quality: 70,
+        reductionEffort: 5,
+      })
+      .toBuffer();
 
     return {
       ...response,
--- worker.js
+++ worker.js
@@ -1,15 +1,15 @@
 import { parentPort, workerData } from 'worker_threads';
 
+import { pipeCache } from './pipes/cache.js';
+import { pipeCharset } from './pipes/charset.js';
 import { pipeCompress } from './pipes/compress.js';
 import { pipeHeadersClean } from './pipes/headersClean.js';
-// import { pipeImage } from './pipes/image.js';
 import { pipeHtml } from './pipes/html.js';
-// import { pipeLosslessImage } from './pipes/losslessImage.js';
-import { pipeCache } from './pipes/cache.js';
+import { pipeImage } from './pipes/image.js';
+import { pipeLosslessImage } from './pipes/losslessImage.js';
+import { pipeLovercaseHeader } from './pipes/lovercaseHeaders.js';
 import { pipeSaveToCache } from './pipes/saveToCache.js';
 import { pipeSvg } from './pipes/svg.js';
-import { pipeLovercaseHeader } from './pipes/lovercaseHeaders.js';
-import { pipeCharset } from './pipes/charset.js';
 
 (async () => {
   const { request, response } = workerData;
@@ -27,8 +27,8 @@ import { pipeCharset } from './pipes/charset.js';
 
   newResponse = await pipeHtml(newResponse, request);
 
-  // newResponse = await pipeImage(newResponse, request);
-  // newResponse = await pipeLosslessImage(newResponse, request);
+  newResponse = await pipeImage(newResponse, request);
+  newResponse = await pipeLosslessImage(newResponse, request);
   newResponse = await pipeSvg(newResponse, request);
 
   newResponse = await pipeCompress(newResponse, request);
