485dfe8f3a8b1d26992635db61be7ae02cfe08a9
Grigorii Horos <horosgrisa@gmail.com>
2020-07-25 00:15:47 +0300

    Fix zpm

--- pipes/html.js
+++ pipes/html.js
@@ -17,6 +17,7 @@ export async function pipeHtml(response, request) {
 
     bodyString = replaceAll(bodyString, '<img', '<img loading="lazy"');
     bodyString = replaceAll(bodyString, '<iframe', '<iframe loading="lazy"');
+    bodyString = replaceAll(bodyString, /(<(?:script|link).*)integrity=(:?'|")sha512-.*?(:?'|")/gm, '$1');
 
     return {
       ...response,
