d998f6b82f0cf4c29cdddc7bc9202f5c2ea42079
Grigorii Horos <horosgrisa@gmail.com>
2023-01-26 23:36:26 +0200

    Change text

--- pipes/lossless-image.js
+++ pipes/lossless-image.js
@@ -24,8 +24,8 @@ const imagemagickArguments = (quality = '30', config = {}) => {
   ];
 
   parameterArguments = config.eink
-    ? [...parameterArguments, '-grayscale', 'Rec709Luma', '-colorspace', 'gray', '-unsharp', '0x2+2+0']
-    : [...parameterArguments, '-colorspace', 'sRGB', '-unsharp', '0x2+1+0', '-gaussian-blur', '0.01'];
+    ? [...parameterArguments, '-median', '2', '-colorspace', 'gray', '-unsharp', '0x2+2+0']
+    : [...parameterArguments, '-median', '1', '-colorspace', 'sRGB', '-unsharp', '0x2+1+0', '-gaussian-blur', '0.01'];
 
   parameterArguments = [...parameterArguments,
     '-define',
